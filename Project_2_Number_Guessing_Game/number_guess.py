4e8b9b8f1e950ecdf8412566ecb0d90c7760775d
Mr-Shubham-Palde <shubhampalde96@gmail.com>
2026-06-17 19:53:17 +0530

    Project 2 updated logic successfully

--- Project_2_Number_Guessing_Game/number_guess.py
+++ Project_2_Number_Guessing_Game/number_guess.py
@@ -2,14 +2,28 @@ import random
 print("Welcome To The Number Guessing Game!")
 number = random.randint(1,100)
 
+chance = 5
+print("You have total ", chance," Chances to guess the number correctly!")
+
 guess = int(input("Guess a number between 1 and 100: "))
 
 while guess!=number:
+    if chance == 0:
+        print("Game Over! You have used all your chances. The correct number was", number)
+        break
+        
+
     if guess>number:
-        print("Too high!Try again and guess lowr")
+        chance-=1
+        print("Too high!Try again and guess lower", "Chance Remaining",chance)
+        
     else:
-        print("Too low!Try again and guess higher")
+        chance-=1
+        print("Too low!Try again and guess higher", "Chance Remaining",chance)
+        
     guess = int(input("Guess a number between 1 and 100: "))
+
     if guess== number:
         print("Congratulations! You guessed the number correctly!")
         break
+    
